58f04b287d9bf636b9b1948eea6c53ed2b2fde2f
brett poarch <brett.poarch@gmail.com>
2017-04-06 13:14:22 -0700

    merge

--- src/components/Brett/App.js
+++ src/components/Brett/App.js
@@ -2,21 +2,12 @@ import React from 'react';
 import InfoSection from './infoSection'
 import Reservation from './reservation'
 
-var styles= {
-	ring: {
-        height: '15px',
-        width: '15px',
-        borderRadius: '50%',
-        border: '6px solid black'
-    }
-  }
 
 export default React.createClass({
   render() {
     return (
       <div>
       	<div>
-      		<img src='https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSNOMca1saenvD9LVXFoGz0AuGassyvjG1K9SOlPDsbOia4IIXx' alt='#'/>
       	</div>
       	<div>
       		<InfoSection />
