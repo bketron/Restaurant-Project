de1b9b887d75a41f67355507bad0b70adb4cb083
brett poarch <brett.poarch@gmail.com>
2017-04-08 11:35:21 -0700

    updating

--- src/components/reservation.js
+++ src/components/reservation.js
@@ -14,15 +14,15 @@ export default React.createClass({
 		this.setState({[e.target.name]: e.target.value})
 	},
 	handleSubmit(e) {
-		e.preventDefault
+		e.preventDefault()
 		addUser(this.state.name, this.state.guests, this.state.date, this.state.notes)
 	},
 	render(){
 		return(
 			<form onSubmit={this.handleSubmit}>
 				<input onChange={this.update} type='text' name='name' placeholder='Name' value={this.state.name} />
-				<input onChange={this.update} type='text' name='guests' placeholder='Number of People' value={this.state.guests} />
-				<input onChange={this.update} type='text' name='date' placeholder='Date' value={this.state.date} />
+				<input onChange={this.update} type='number' name='guests' placeholder='Number of People' value={this.state.guests} />
+				<input onChange={this.update} type='date' name='date' placeholder='Date' value={this.state.date} />
 				<input onChange={this.update} type='text' name='notes' placeholder='Special Requests' value={this.state.notes} />
 				<button type='submit'>Reserve Table</button>
 			</form>
